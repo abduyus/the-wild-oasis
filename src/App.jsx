546cc76dc79e72caefae573eea319161908aea10
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-01-12 18:35:11 +0000

    adding styles with styled components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles.js";
+
+
+const H1 = styled.h1`
+    
+`
+
+const Button = styled.button`
+    background-color: var(--color-brand-600);
+    
+`
+
+const Input = styled.input`
+   
+`
+
+const StyledApp = styled.div`
+  
+
+`
+
+
+
 
 function App() {
     return (
-        <div>Hello World</div>
+        <>
+        <GlobalStyles />
+        <StyledApp>
+
+        <H1>The Wild Oasis</H1>
+        <Button>Check In</Button>
+        <Input type={'number'} placeholder={'Number of guests'}/>
+        </StyledApp>
+        </>
     );
 }
 
