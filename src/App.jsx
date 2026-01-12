bd728e8d29a7583c6fc901a19dbb0ef0dca4d749
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-01-12 19:08:00 +0000

    adding globalstyles

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,30 @@ import GlobalStyles from "./styles/GlobalStyles.js";
 
 
 const H1 = styled.h1`
-    
+    font-size: 30px;
+    font-weight: 600;
 `
 
 const Button = styled.button`
-    background-color: var(--color-brand-600);
-    
+    font-size: 1.4rem;
+    padding: 1.2rem 1.6rem;
+    font-weight: 500;
+    border: none;
+    border-radius: 7px;
+    background-color: var(--color-brand-500);
+    color: #fff;
+    cursor: pointer;
 `
 
 const Input = styled.input`
-   
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    padding: 0.8rem 1.2rem
 `
 
 const StyledApp = styled.div`
-  
+    padding: 20px;
+    border-radius: 15px;
 
 `
 
@@ -26,15 +36,13 @@ const StyledApp = styled.div`
 
 function App() {
     return (
-        <>
-        <GlobalStyles />
-        <StyledApp>
 
-        <H1>The Wild Oasis</H1>
-        <Button>Check In</Button>
-        <Input type={'number'} placeholder={'Number of guests'}/>
-        </StyledApp>
-        </>
+            <StyledApp>
+
+            <H1>The Wild Oasis</H1>
+            <Button>Check In</Button>
+            <Input type={'number'} placeholder={'Number of guests'}/>
+            </StyledApp>
     );
 }
 
