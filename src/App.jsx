ccc99b229aa4e46795ab7ab6b14bd4f2e8dbe6ef
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-01-12 19:18:23 +0000

    refactoring Button and Input components with styles to seperate files in UI folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
 import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles.js";
+import Button from "./ui/Button.jsx";
+import Input from "./ui/Input.jsx";
+
 
 
 const H1 = styled.h1`
     font-size: 30px;
     font-weight: 600;
-`
-
-const Button = styled.button`
-    font-size: 1.4rem;
-    padding: 1.2rem 1.6rem;
-    font-weight: 500;
-    border: none;
-    border-radius: 7px;
-    background-color: var(--color-brand-500);
-    color: #fff;
-    cursor: pointer;
-`
-
-const Input = styled.input`
-    border: 1px solid #ddd;
-    border-radius: 5px;
-    padding: 0.8rem 1.2rem
 `
 
 const StyledApp = styled.div`
@@ -32,16 +17,13 @@ const StyledApp = styled.div`
 `
 
 
-
-
 function App() {
     return (
 
             <StyledApp>
-
-            <H1>The Wild Oasis</H1>
-            <Button>Check In</Button>
-            <Input type={'number'} placeholder={'Number of guests'}/>
+                <H1>The Wild Oasis</H1>
+                <Button>Check In</Button>
+                <Input type={'number'} placeholder={'Number of guests'}/>
             </StyledApp>
     );
 }
