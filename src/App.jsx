62e5f05ca15ca16905d83d0373c02b9eaa23a5d3
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-01-13 21:29:53 +0000

    building reusable styles components with default props

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 import Button from "./ui/Button.jsx";
 import Input from "./ui/Input.jsx";
-
-
-
-const H1 = styled.h1`
-    font-size: 30px;
-    font-weight: 600;
-`
+import Heading from "./ui/Heading.jsx";
+import Row from "./ui/Row.jsx";
 
 const StyledApp = styled.div`
-    padding: 20px;
-    border-radius: 15px;
-
-`
-
+  padding: 20px;
+  border-radius: 15px;
+`;
 
 function App() {
-    return (
-
-            <StyledApp>
-                <H1>The Wild Oasis</H1>
-                <Button>Check In</Button>
-                <Input type={'number'} placeholder={'Number of guests'}/>
-            </StyledApp>
-    );
+  return (
+    <StyledApp>
+      <Row type={"horizontal"}>
+        <Heading as={"h1"}>The Wild Oasis</Heading>
+        <div>
+          <Heading as={"h2"}>Form</Heading>
+          <Button>Check In</Button>
+          <Button variation={"secondary"} size={"small"}>
+            Check In
+          </Button>
+        </div>
+      </Row>
+      <Row>
+        <Heading as={"h3"}>Check in and out</Heading>
+        <form>
+          <Input type={"number"} placeholder={"Number of guests"} />
+        </form>
+      </Row>
+    </StyledApp>
+  );
 }
 
 export default App;
