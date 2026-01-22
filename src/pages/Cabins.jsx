3f1076ff66746f41d138da69ab1605c2becc8286
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-01-22 11:20:32 +0000

    displaying the add new cabin form with react hook form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,14 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getCabins } from "../services/apiCabins.js";
 import log from "eslint-plugin-react/lib/util/log.js";
 import CabinTable from "../features/cabins/CabinTable.jsx";
+import Button from "../ui/Button.jsx";
+import CreateCabinForm from "../features/cabins/CreateCabinForm.jsx";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -15,6 +18,8 @@ function Cabins() {
 
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm(!showForm)}>Add new Cabin</Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
