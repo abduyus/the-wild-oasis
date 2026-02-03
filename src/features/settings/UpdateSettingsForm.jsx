f74adbbc40e964a47ee0ff743856f45ebd9c11a6
Yusuf Abdur-Rasheed <yusuf.abdurrasheed@outlook.com>
2026-02-03 19:31:21 +0000

    allowing updating of application settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -4,6 +4,7 @@ import Input from "../../ui/Input";
 import { useSettings } from "./useSettings.js";
 import settings from "../../pages/Settings.jsx";
 import Spinner from "../../ui/Spinner.jsx";
+import { useUpdateSetting } from "./useUpdateSetting.js";
 
 function UpdateSettingsForm() {
   const {
@@ -15,8 +16,15 @@ function UpdateSettingsForm() {
       breakfastPrice,
     } = {},
   } = useSettings();
+  const { isUpdating, updateSetting } = useUpdateSetting();
 
   if (isLoading) return <Spinner />;
+
+  function handleUpdate(e, field) {
+    const { value } = e.target;
+    if (!value) return;
+    updateSetting({ [field]: value });
+  }
 
   return (
     <Form>
@@ -24,24 +32,36 @@ function UpdateSettingsForm() {
         <Input
           type="number"
           id="min-nights"
+          disabled={isUpdating}
           defaultValue={minimumBookingLength}
+          onBlur={(e) => handleUpdate(e, "minimumBookingLength")}
         />
       </FormRow>
+
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          type="number"
+          id="max-nights"
+          defaultValue={maxBookingLength}
+          onBlur={(e) => handleUpdate(e, "maxBookingLength")}
+        />
       </FormRow>
+
       <FormRow label="Maximum guests/booking">
         <Input
           type="number"
           id="max-guests"
           defaultValue={maxGuestsPerBooking}
+          onBlur={(e) => handleUpdate(e, "maxGuestsPerBooking")}
         />
       </FormRow>
+
       <FormRow label="Breakfast price">
         <Input
           type="number"
           id="breakfast-price"
           defaultValue={breakfastPrice}
+          onBlur={(e) => handleUpdate(e, "breakfastPrice")}
         />
       </FormRow>
     </Form>
